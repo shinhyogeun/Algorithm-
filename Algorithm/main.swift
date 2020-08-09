b27d12db71503b34c1d10e108a0d9135aa0b0d80
shinhyogeun <tls1gy2rms3@naver.com>
2020-08-09 14:03:48 +0900

    8/9 Algorithm update

--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -54,12 +54,76 @@
 
 //A+B +5 문제
 
-import Foundation
-//
+//import Foundation
 //while let line = readLine() {
 //    let realLine = line.split(separator: " ")
 //    let a = Int(realLine[0])!
 //    let b = Int(realLine[1])!
 //    print(a+b)
 //}
-while let line = readLine() { print(line) }
+
+//A+B -4 문제
+//import Foundation
+//
+//let A = Int(readLine()!)!
+//
+//var B : Int?
+//var C : Int = A
+//
+//var count = 0
+//
+//while A != B {
+//    count+=1
+//    if C < 10 {
+//        B = 10*C + C
+//    } else {
+//        B = (C%10)*10 + (C/10+C%10)%10
+//    }
+//    C = B!
+//}
+//
+//print(count)
+
+//평균구하기
+//import Foundation
+//
+//let A = Int(readLine()!)!
+//let B = Int(readLine()!)!
+//let C = Int(readLine()!)!
+//let D = Int(readLine()!)!
+//let E = Int(readLine()!)!
+//
+//var Arr = [A,B,C,D,E]
+//
+//var total = 0
+//for i in 0...Arr.count-1{
+//    if Arr[i] < 40 {
+//        total += 40
+//    } else {
+//        total += Arr[i]
+//    }
+//
+//    if i == Arr.count - 1 {
+//        total = total / Arr.count
+//        print(total)
+//    }
+//}
+
+//상근날드
+
+import Foundation
+
+let A = Int(readLine()!)!
+let B = Int(readLine()!)!
+let C = Int(readLine()!)!
+let D = Int(readLine()!)!
+let E = Int(readLine()!)!
+
+var burgerArr = [A,B,C]
+var beverageArr = [D,E]
+
+let real1 = burgerArr.min()
+let real2 = beverageArr.min()
+
+print(real1! + real2! - 50)
+
